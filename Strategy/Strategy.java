01a6416ef5f9f1b9429b94de30b970da15ba0763
Amisha Agarwal <amisha.agarwal29@gmail.com>
2016-12-05 01:05:23 +0530

    Strategy pattern uploaded

--- Strategy/Strategy.java
+++ Strategy/Strategy.java
@@ -1,20 +1,12 @@
 
 import java.util.*;
 
-/**
- *
- */
 public interface NameList {
 
 
-    /**
-     * @return
-     */
     public void summarize();
 
-    /**
-     *
-     */
+   
     public void listOfNames(HashMap< Integer, ArrayList<String>>  hmap );
 
 }
@@ -29,18 +21,13 @@ public class SummarizeName implements NameList {
 
     ArrayList<String> detail = new ArrayList<>();;
     String[] name;
-    /**
-     * Default constructor
-     */
+    
     public SummarizeName() {
 
     }
 
 
 
-    /**
-     * @return
-     */
     public void summarize() {
         // TODO implement here
         Arrays.sort(name);
@@ -51,9 +38,7 @@ public class SummarizeName implements NameList {
 
     }
 
-    /**
-     *
-     */
+   
     public void listOfNames(HashMap< Integer, ArrayList<String>>  hmap ) {
         // TODO implement here
         name= new String[hmap.size()];
@@ -71,28 +56,18 @@ public class SummarizeName implements NameList {
 
 import java.util.*;
 
-/**
- *
- */
 public class SummarizeOrganization implements NameList {
 
-    /**
-     * Default constructor
-     */
     public SummarizeOrganization() {
     }
 
-    /**
-     * @return
-     */
+  
     public void summarize() {
         // TODO implement here
 
     }
 
-    /**
-     *
-     */
+ 
     public void listOfNames(HashMap< Integer, ArrayList<String>>  hmap () {
         // TODO implement here
     }
@@ -103,14 +78,8 @@ public class SummarizeOrganization implements NameList {
 
 import java.util.*;
 
-/**
- *
- */
 public abstract class Contact {
 
-    /**
-     * Default constructor
-     */
     public Contact() {
     }
 
@@ -121,31 +90,14 @@ public abstract class Contact {
           hashmap = new  HashMap<>();
     }
 
-    /**
-     *
-     */
+   
     public int i;
 
-    /**
-     *
-     */
-    public NameList name;
 
-    /**
-     *
-     */
     public HashMap<Integer, ArrayList<String>> hashmap;
 
-
-
-    /**
-     * @return
-     */
     public abstract void display();
 
-    /**
-     * @return
-     */
     public void addDetail(String s1, String s2) {
         // TODO implement here
         ArrayList<String> al= new ArrayList<>();
@@ -162,32 +114,22 @@ public abstract class Contact {
 
 import java.util.*;
 
-/**
- *
- */
 public class ContactList1 extends Contact {
 
     static NameList nam = new SummarizeName();
-    /**
-     * Default constructor
-     */
+  
     public ContactList1() {
         super(nam);
     }
 
-    /**
-     * @return
-     */
+ 
     public void display() {
         // TODO implement here
         name.summarize();
 
     }
 
-    /**
-     * @return
-     */
-
+ 
 
 }
 
@@ -195,21 +137,14 @@ public class ContactList1 extends Contact {
 
 import java.util.*;
 
-/**
- *
- */
 public class ContactList2 extends Contact {
     static NameList nam = new SummarizeOrganization();
-    /**
-     * Default constructor
-     */
+ 
     public ContactList2() {
       super(nam);
     }
 
-    /**
-     * @return
-     */
+   
     public void display() {
         // TODO implement here
         name.summarize();
@@ -224,14 +159,9 @@ public class ContactList2 extends Contact {
 
 import java.util.*;
 
-/**
- *
- */
 public class Client {
 
-    /**
-     * Default constructor
-     */
+  
     public Client() {
     }
 
@@ -244,9 +174,7 @@ public class Client {
        a.addDetail("Suresh","Company2");
        a.addDetail("Abhi","Company2");
        a.display();
-   //Contact b =new ContactList2();
-       //b.displayContact();
-
+  
    }
 
 }
